496d3d4761efe97eff7c618bba68a82707b7d82b
Winnie Jeng <winniewjeng@gmail.com>
2018-08-20 23:55:19 -0700

    boat objects not initialized correctly

--- Boat.java
+++ Boat.java
@@ -11,45 +11,68 @@
 //file “boats.txt”. You can assume there will be a maximum of 100 boats in the file.
 //For testing, use the following boat names in the boats.txt file:
 
-import java.util.Scanner;
-import java.io.*;
-        
 public class Boat {
-    
+
+    private int speed;
+    private int angle;
+    private boolean power;
     private String name;
-    private String command;
-    private int degree = 0;
-    private int speed = 0;
-    private Boolean powerOn = false;
-    
-    public Boat(String name, String command) {
-        name = this.name;
 
-        if (powerOn == false && command != "power on") {
-            System.out.println("Please power on your boat first.");
-        }
-        
-        else {
-            command = this.command;
-            //Call method
-        }
-    }
-    
-    Boolean powerOn() {
-        powerOn = true;
-        return true;
-    }
-    
-    Boolean powerOff() {
-        powerOn = false;
-        degree = 0;
+    public Boat(String aName) {
         speed = 0;
-        System.out.println(name + " is powered off.");
-        return false;
+        angle = 0;
+        power = false;
+        name = aName;
     }
-    
-    
-    
 
-    
+    public void PowerOn() {
+        power = true;
+        System.out.println(toString());
+    }
+
+    public void PowerOff() {
+//        System.out.println("off now " + power);
+        power = false;
+        speed = 0;
+        angle = 0;
+        System.out.println(toString());
+    }
+
+    public void speedUp() {
+        speed += 2;
+    }
+
+    public void slowDown() {
+        if (speed - 2 < 0) {
+            System.out.println("Cannot slow down any more.");
+        } else {
+            speed -= 2;
+        }
+    }
+
+    public void turnLeft() {
+        angle -= 5;
+        System.out.println(toString());
+    }
+
+    public void turnRight() {
+        angle += 5;
+        System.out.println(toString());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        if (power == false) {
+            return getName() + "'s power is off";
+        }
+        if (speed == 0) {
+            return getName() + " is pointing in the direction of " + angle + " degree.";
+        }
+        return getName() + " is moving in " + angle + " deg angle at the speed of " + speed + " mph.";
+    }
+
 }
